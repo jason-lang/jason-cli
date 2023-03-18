7db5c3b27a05290f3952b8d566e4032e71516699
Jomi Fred Hubner <jomifred@gmail.com>
2023-03-18 23:51:41 +0100

    rec

--- src/main/java/jason/cli/mas/MAS.java
+++ src/main/java/jason/cli/mas/MAS.java
@@ -1,5 +1,10 @@
 package jason.cli.mas;
 
+import java.io.File;
+import java.io.FileReader;
+import java.net.Socket;
+import java.util.Properties;
+
 import picocli.CommandLine.Command;
 
 @Command(
@@ -8,5 +13,38 @@ import picocli.CommandLine.Command;
     subcommands = { Start.class, Stop.class }
 )
 public class MAS { 
+
+    Socket getRunningMAS(String masName) {
+        var props = new Properties();
+        var f = new File(System.getProperty("java.io.tmpdir") + CommandServer.RUNNING_MAS_FILE_NAME);
+        if (f.exists()) {
+            try {
+                props.load(new FileReader(f));
+                var addr = "";
+                if (masName.isEmpty()) {
+                    masName = props.getProperty("latest___mas", "");
+                    if (!masName.isEmpty()) {
+                        addr = props.getProperty(masName, "");
+                    }
+                } else {
+                    addr = props.getProperty(masName, "");
+                }
+                System.out.println("(trying to) stop MAS "+masName+" at "+addr);
+                if (!addr.isEmpty()) {
+                    // try to connect
+                    var saddr = addr.split(":");
+                    var host = saddr[0];
+                    var port = Integer.parseInt(saddr[1]);
+
+                    return new Socket(host, port);                        
+                }
+
+            } catch (Exception e) {
+                // e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 }
 
--- src/main/java/jason/cli/mas/Stop.java
+++ src/main/java/jason/cli/mas/Stop.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ParentCommand;
 
 
 @Command(
@@ -21,10 +22,12 @@ public class Stop implements Runnable {
                description = "MAS unique identification")
     String masName;
 
+    @ParentCommand
+    private MAS parent;
     
     @Override
     public void run() {
-        var s = getRunningMAS(masName);
+        var s = parent.getRunningMAS(masName);
         if (s != null) {
             try (var out = new PrintWriter(s.getOutputStream(), true)) {
                 out.println("stop");
@@ -42,37 +45,6 @@ public class Stop implements Runnable {
 
     }
 
-    Socket getRunningMAS(String masName) {
-        var props = new Properties();
-        var f = new File(System.getProperty("java.io.tmpdir") + CommandServer.RUNNING_MAS_FILE_NAME);
-        if (f.exists()) {
-            try {
-                props.load(new FileReader(f));
-                var addr = "";
-                if (masName.isEmpty()) {
-                    masName = props.getProperty("latest___mas", "");
-                    if (!masName.isEmpty()) {
-                        addr = props.getProperty(masName, "");
-                    }
-                } else {
-                    addr = props.getProperty(masName, "");
-                }
-                System.out.println("(trying to) stop MAS "+masName+" at "+addr);
-                if (!addr.isEmpty()) {
-                    // try to connect
-                    var saddr = addr.split(":");
-                    var host = saddr[0];
-                    var port = Integer.parseInt(saddr[1]);
-
-                    return new Socket(host, port);                        
-                }
-
-            } catch (Exception e) {
-                // e.printStackTrace();
-            }
-        }
-        return null;
-    }
 
 }
 

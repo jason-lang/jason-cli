7faa755290943478882c9e2d54e3732af688cb66
Jomi Fred Hubner <jomifred@gmail.com>
2023-03-24 23:18:29 +0100

    Update RunningMASs.java

--- src/main/java/jason/cli/mas/RunningMASs.java
+++ src/main/java/jason/cli/mas/RunningMASs.java
@@ -15,8 +15,6 @@ import java.util.Properties;
 /** handle the current/known MAS that are running */
 public class RunningMASs {
 
-    public static String RUNNING_MAS_FILE_NAME = "jason-cmd-server";
-
     protected static CLILocalMAS localRunningMAS = null;
 
     public static void setLocalRunningMAS(CLILocalMAS r) {
@@ -49,9 +47,26 @@ public class RunningMASs {
         }
         //  find remote runtime service by RMI
         try {
-            return (RuntimeServices) LocateRegistry.getRegistry().lookup(RunLocalMAS.RMI_PREFIX_RTS+masName);
+            var rt = (RuntimeServices) LocateRegistry.getRegistry().lookup(RunLocalMAS.RMI_PREFIX_RTS+masName);
+            if (rt.isRunning()) {
+                return rt;
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+        }
+
+        //  try with list of running MAS
+        try {
+            var addr = getAllRunningMAS().get(masName);
+            if  (addr != null && !addr.isEmpty()) {
+                // try to connect
+                var saddr = addr.split(":");
+                var rt = (RuntimeServices)LocateRegistry.getRegistry(saddr[0], Integer.parseInt(saddr[1])).lookup(RunLocalMAS.RMI_PREFIX_RTS+masName);
+
+                if (rt.isRunning()) {
+                    return rt;
+                }
+            }
+        } catch (Exception e) {
         }
 
         return null;
@@ -71,12 +86,12 @@ public class RunningMASs {
 
     public static Map<String,String> getAllRunningMAS() {
         var map = getRMIRunningMAS();
-        /*var all = testAllRemoteMAS();
+        var all = testAllRemoteMAS();
         for (var mas: all.keySet()) {
             if (mas.equals("latest___mas"))
                 continue;
             map.put(mas.toString(), all.getProperty(mas.toString()));
-        }*/
+        }
         if (isRunningMAS(null)) {
             map.put(localRunningMAS.getProject().getSocName(), "local");
         }
@@ -99,9 +114,10 @@ public class RunningMASs {
         }
         return map;
     }
-    /*public static Properties testAllRemoteMAS() {
+
+    public static Properties testAllRemoteMAS() {
         var props = new Properties();
-        var f = getRunningMASFile();
+        var f = RunLocalMAS.getRunningMASFile();
         if (f.exists()) {
             try {
                 props.load(new FileReader(f));
@@ -121,19 +137,14 @@ public class RunningMASs {
                 var saddr = addr.split(":");
                 try {
                     // try to connect
-                    var s = new Socket(saddr[0], Integer.parseInt(saddr[1]));
-                    var out = new PrintWriter(s.getOutputStream(), true);
-                    var in  = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                    out.println("mas__running");
-                    if (in.readLine().equals("no")) {
-                        //System.out.println("    (removing "+masName+" from cache)");
+                    //System.out.println("** try "+addr+" for "+masName);
+                    var rt = (RuntimeServices)LocateRegistry.getRegistry(saddr[0], Integer.parseInt(saddr[1])).lookup(RunLocalMAS.RMI_PREFIX_RTS+masName);
+
+                    if (!rt.isRunning()) {
                         props.remove(masName);
                         changed = true;
                     }
-                    s.close();
                 } catch (Exception e) {
-                    // e.printStackTrace();
-                    //System.out.println("    (removing "+masName+" from cache)");
                     props.remove(masName);
                     changed = true;
                 }
@@ -141,69 +152,13 @@ public class RunningMASs {
 
             if (changed) {
                 try {
-                    props.store(new FileWriter(f),"running mas in jason");
+                    // TODO uncomment props.store(new FileWriter(f),"running mas in jason");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
         return props;
-    }*/
-
-    /*public static File getRunningMASFile() {
-        return new File(System.getProperty("java.io.tmpdir") + RUNNING_MAS_FILE_NAME);
-    }*/
-
-    /*public static Socket getRemoteRunningMAS(String masName) {
-        var props = new Properties();
-        var f = getRunningMASFile();
-        if (f.exists()) {
-            try {
-                props.load(new FileReader(f));
-                var addr = "";
-                if (masName.isEmpty()) {
-                    masName = props.getProperty("latest___mas", "");
-                    if (!masName.isEmpty()) {
-                        addr = props.getProperty(masName, "");
-                    }
-                } else {
-                    addr = props.getProperty(masName, "");
-                }
-//                System.out.println("add found at "+addr);
-                if (!addr.isEmpty()) {
-                    // try to connect
-                    var saddr = addr.split(":");
-                    var s = new Socket(saddr[0], Integer.parseInt(saddr[1]));
-//                    System.out.println("returning s "+s);
-                    return s;
-                }
-            } catch (Exception e) {
-//                 e.printStackTrace();
-            }
-        }
-        return null;
-    }*/
-
-    /*public static File storeRunningMAS() {
-        if (localRunningMAS == null)
-            return null;
-        try {
-            var props = new Properties();
-
-            var f = getRunningMASFile();
-            if (f.exists()) {
-                props.load(new FileReader(f));
-            }
-            var masName = localRunningMAS.getName();
-            props.put("latest___mas", masName);
-            props.store(new FileWriter(f),"running mas in jason");
-            // System.out.println("store server data in "+f.getAbsolutePath());
-            return f;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }*/
-
+    }
 }
 
